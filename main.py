74fc0e31f3e399635c03bf611531e1395f41ce78
macit1 <54212915+macit1@users.noreply.github.com>
2023-09-07 09:50:54 +0200

    without print

--- main.py
+++ main.py
@@ -17,6 +17,3 @@ print("The original list : " + str(test_list))
 test_list.sort()
 mid = len(test_list) // 2
 res = (test_list[mid] + test_list[~mid]) / 2
-
-# Printing result
-print("Median of list is : " + str(res))
